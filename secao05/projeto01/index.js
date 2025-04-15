22348c83400797a48e20316455462bad683eaa09
Vitor Lima <viitorlima2000@gmail.com>
2025-04-14 21:03:17 -0300

    Moving files

--- secao05/projeto01/index.js
+++
@@ -1,8 +0,0 @@
-let userValue = prompt('Enter the product value: ');
-
-if (userValue < 20 || (typeof userValue) === 'string') {
-    document.getElementById('result').innerHTML = 'Denied'
-}
-else {
-    document.getElementById('result').innerHTML = 'Approved'
-}
